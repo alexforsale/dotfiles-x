92b6d58ebf61473c767a15d5b5b140a7f26a4457
alexforsale <alexforsale@yahoo.com>
2021-03-25 23:56:49 +0700

    pgrep: only search ${USER} process

--- .config/xprofile.d/00-security.sh
+++ .config/xprofile.d/00-security.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# ~/.config/xprofile.d/00-security.sh
+# various ui-related security settings
+# <alexforsale@yahoo.com>
 
 if [ "$(command -v gnome-keyring-daemon)" ];then
     eval "$(/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh)"
@@ -12,18 +15,18 @@ case "${DISTRO}" in
             export SUDO_ASKPASS=/usr/bin/x11-ssh-askpass
 
         # polkit
-        if [ ! "$(pgrep -x polkit-gnome-authentication-agent-1)" ];then
+        if [ ! "$(pgrep -u ${USER} -x polkit-gnome-authentication-agent-1)" ];then
             sleep 1 && /usr/libexec/polkit-gnome-authentication-agent-1 &
         fi
         ;;
     arch)
         if [ -f /usr/lib/ssh/x11-ssh-askpass ];then
-            ln -s /usr/lib/ssh/x11-ssh-askpass ~/.local/bin/ssh-askpass
+            ln -s /usr/lib/ssh/x11-ssh-askpass ~/.local/bin/ssh-askpass &
             export SSH_ASKPASS=ssh-askpass
             export SUDO_ASKPASS="${HOME}"/.local/bin/ssh-askpass
         fi
         # polkit
-        if [ ! "$(pgrep -x polkit-gnome-authentication-agent-1)" ];then
+        if [ ! "$(pgrep -u ${USER} -x polkit-gnome-authentication-agent-1)" ];then
             sleep 1 && /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
         fi
 
@@ -37,7 +40,7 @@ case "${DISTRO}" in
             export SUDO_ASKPASS=/usr/local/bin/x11-ssh-askpass
 
         # polkit
-        if [ ! "$(pgrep -x polkit-gnome-authentication-agent-1)" ];then
+        if [ ! "$(pgrep -u ${USER} -x polkit-gnome-authentication-agent-1)" ];then
             sleep 1 && /usr/local/libexec/polkit-gnome-authentication-agent-1 &
         fi
         ;;
