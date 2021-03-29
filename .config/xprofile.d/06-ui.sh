24ffd5dab32ca67d93f3d9ecfef0dea5d948dbf4
alexforsale <alexforsale@yahoo.com>
2021-03-30 03:29:25 +0700

    handles monitor naming

--- .config/xprofile.d/06-ui.sh
+++ .config/xprofile.d/06-ui.sh
@@ -2,6 +2,14 @@
 # ~/.config/xprofile.d/06-ui.sh
 # various ui settings
 
+# monitor
+_count=0
+for i in $(xrandr | grep " connected"| awk '{print $1}');do
+    _count=$((_count+1))
+    export MONITOR${_count}=${i}
+done
+unset _count
+
 # wallpaper
 if [ -e ~/.local/bin/wallpaper ]; then
     ~/.local/bin/setwallpaper &
